4afe5ac66907fe8109ad389595a89795a9a577dc
Eric Gauderman <eric.gauderman@gmail.com>
2016-12-02 15:31:35 -0800

    Homepage basic info

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import App from './App';
 import Home from './pages/Home';
@@ -11,7 +11,7 @@ import './index.css';
 import 'bootstrap-css-only';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path="prizes" component={Prizes}/>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,15 +7,44 @@ class Home extends Component {
     return (
       <div className="Home">
         <div className="container">
-          <p>
-            The USC Glamour Smackdown is the most extravagant of all collegiate dancesport
-            competitions! Bring your boas, bling, bow ties, and most importantly
-            your best ballroom dancing to knock out your opponents! Come,
-            dance, win! 
-          </p>
-          <p>
-            Let the countdown to the smackdown begin!
-          </p>
+          <div>
+            <div className="row">
+              <h4 className="col-sm-2 Home-question">When?</h4>
+              <p className="col-sm-10">February 18, 2017</p>
+            </div>
+            <div className="row">
+              <h4 className="col-sm-2 Home-question">Where?</h4>
+              <p className="col-sm-10">Tutor Campus Center Grand Ballroom at the University of Southern California</p>
+            </div>
+            <div className="row">
+              <h4 className="col-sm-2 Home-question">What?</h4>
+              <p className="col-sm-10">Collegiate Ballroom Dance competition</p>
+            </div>
+            <p>
+              The USC Glamour Smackdown is the most extravagant of all collegiate dancesport
+              competitions! Bring your boas, bling, bow ties, and most importantly
+              your best ballroom dancing to knock out your opponents!
+            </p>
+            <p>
+              Let the countdown to the smackdown begin!
+            </p>
+            <p>
+              About the theme: Dancesport sits at the crossroads of two wildly different ideas.
+              It's elegant, but fierce; cordial, but cutthroat. Glamour Smackdown
+              is about maxing out these contrasts to give competitors the ultimate
+              dancesport experience.
+            </p>
+            <h2>Registration Deadline: Friday,&nbsp;Feb.&nbsp;3,&nbsp;2017</h2>
+            <p>Late Registration Deadline: Friday,&nbsp;Feb.&nbsp;10,&nbsp;2017</p>
+            <h2>Registration Price</h2>
+            <h4>All events: $30</h4>
+            <h4>Nightclub and/or Team Performance events only: $15</h4>
+            <h4>Late Registration:</h4>
+            <p>All events: $30</p>
+            <p>Nightclub and/or Team Performance $20</p>
+            <h2>Spectator Admission&nbsp;Price</h2>
+            <h5>$10</h5>
+          </div>
         </div>
       </div>
     );
