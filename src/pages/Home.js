2ecf14f2e081e8dcb4e48967dc0fb0224607c916
Eric Gauderman <eric.gauderman@gmail.com>
2016-12-06 01:05:47 -0800

    Home styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,41 +9,71 @@ class Home extends Component {
         <div className="Home-top-section">
           <div className="container">
             <div className="Home-row">
-              <p className="Home-heading">WHEN</p>
-              <p className="Home-info">Saturday February 18, 2017</p>
+              <div className="Home-title">WHEN</div>
+              <div className="Home-info">
+                <p>
+                  Saturday February 18, 2017
+                </p>
+              </div>
             </div>
-            <p className="Home-heading">WHERE</p>
-            <p className="Home-info">Tutor Campus Center Grand Ballroom at the University of Southern California</p>
-            <p className="Home-heading">WHAT</p>
-            <p>
-              The USC Glamour Smackdown is the most extravagant of all collegiate dancesport
-              competitions! Bring your boas, bling, bow ties, and most importantly
-              your best ballroom dancing to knock out your opponents!
-            </p>
-            <p>
-              Let the countdown to the smackdown begin!
-            </p>
-            <p>
-              About the theme: Dancesport sits at the crossroads of two wildly different ideas.
-              It's elegant, but fierce; cordial, but cutthroat. Glamour Smackdown
-              is about maxing out these contrasts to give competitors the ultimate
-              dancesport experience.
-            </p>
-          </div>
-        </div>
-        <div className="container">
-          <div>
-            <h2>Registration Deadline: Friday,&nbsp;Feb.&nbsp;3,&nbsp;2017</h2>
-            <h2>Registration Price</h2>
-            <h4>All events: $30</h4>
-            <h4>Nightclub and/or Team Performance events only: $15</h4>
-            <h2>Spectator Admission&nbsp;Price</h2>
-            <h4>$10</h4>
-            <h2>Late Registration</h2>
-            <h4>Late Registration Deadline: Friday,&nbsp;Feb.&nbsp;10,&nbsp;2017</h4>
-            <h4>Late Registration Price:</h4>
-            <p>All events: $40</p>
-            <p>Nightclub and/or Team Performance: $20</p>
+            <div className="Home-row">
+              <div className="Home-title">WHERE</div>
+              <div className="Home-info">
+                <p>
+                  <strong>Tutor Campus Center Grand Ballroom</strong><br/>
+                  University of Southern California<br/>
+                  3607 Trousdale Pkwy<br/>
+                  Los Angeles, CA 90089
+                </p>
+              </div>
+            </div>
+            <div className="Home-row">
+              <div className="Home-title">WHAT</div>
+              <div className="Home-info">
+                <p>
+                  The USC Glamour Smackdown is the most extravagant of all collegiate dancesport
+                  competitions! Bring your boas, bling, bow ties, and most importantly
+                  your best ballroom dancing to knock out your opponents!
+                </p>
+                <p>
+                  Let the countdown to the smackdown begin!
+                </p>
+              </div>
+            </div>
+            <div className="Home-row">
+              <div className="Home-title">ABOUT THE THEME</div>
+              <div className="Home-info">
+                <p>
+                  Dancesport sits at the crossroads of two wildly different ideas.
+                  It's elegant, but fierce; cordial, but cutthroat. Glamour Smackdown
+                  is about maxing out these contrasts to give competitors the ultimate
+                  dancesport experience.
+                </p>
+              </div>
+            </div>
+            <div className="Home-row">
+              <div className="Home-title">REGISTRATION</div>
+              <div className="Home-info">
+                <p><strong>Registration Deadline: Friday,&nbsp;Feb.&nbsp;3,&nbsp;2017</strong></p>
+                <p><a href="" className="Home-register-link" target="_blank">Register now</a></p>
+                <p>
+                  <strong>Prices</strong>
+                  <ul>
+                    <li><b>$30</b> All events</li>
+                    <li><b>$15</b> Nightclub and/or Performance Team events only</li>
+                    <li><b>$10</b> Spectator admission (at the door)</li>
+                  </ul>
+                </p>
+                <p>
+                  <strong>Late Registration</strong>
+                  <ul>
+                    <li>Late deadline: <b>Friday,&nbsp;Feb.&nbsp;10,&nbsp;2017</b></li>
+                    <li><b>$40</b> All events</li>
+                    <li><b>$20</b> Nightclub and/or Performance Team events only</li>
+                  </ul>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
