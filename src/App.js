fd1bc14e36c0cbc2aee9e8812f136db3fb4bb8d0
Eric Gauderman <eric.gauderman@gmail.com>
2016-12-11 17:09:36 -0800

    CSS cleanup and vertically compress the nav when the nav links wrap around

--- src/App.js
+++ src/App.js
@@ -14,29 +14,25 @@ class App extends Component {
           <div className="container">
             <img src={heroImage} alt="Glamour Smackdown" className="App-header-image"/>
           </div>
-          <div className="App-navigation-full-width">
-            <div className="container App-navigation-container">
-              <div className="App-navigation-capsule">
-                <nav className="App-navigation">
-                  <IndexLink to="/" className="App-navigation-link" activeClassName="App-navigation-active">
-                    home
-                  </IndexLink>
-                  <Link to="/travel" className="App-navigation-link" activeClassName="App-navigation-active">
-                    location&nbsp;&amp;&nbsp;travel
-                  </Link>
-                  <Link to="/events" className="App-navigation-link" activeClassName="App-navigation-active">
-                    events&nbsp;&amp;&nbsp;schedule
-                  </Link>
-                  <Link to="/rules" className="App-navigation-link" activeClassName="App-navigation-active">
-                    rules
-                  </Link>
-                  <Link to="/contact" className="App-navigation-link" activeClassName="App-navigation-active">
-                    contact
-                  </Link>
-                </nav>
-              </div>
-            </div>
-            <div className="App-navigation-mobile">
+          <div className="container App-navigation-container">
+            <div className="App-navigation-capsule">
+              <nav className="App-navigation">
+                <IndexLink to="/" className="App-navigation-link" activeClassName="App-navigation-active">
+                  home
+                </IndexLink>
+                <Link to="/travel" className="App-navigation-link" activeClassName="App-navigation-active">
+                  location&nbsp;&amp;&nbsp;travel
+                </Link>
+                <Link to="/events" className="App-navigation-link" activeClassName="App-navigation-active">
+                  events&nbsp;&amp;&nbsp;schedule
+                </Link>
+                <Link to="/rules" className="App-navigation-link" activeClassName="App-navigation-active">
+                  rules
+                </Link>
+                <Link to="/contact" className="App-navigation-link" activeClassName="App-navigation-active">
+                  contact
+                </Link>
+              </nav>
             </div>
           </div>
         </div>
